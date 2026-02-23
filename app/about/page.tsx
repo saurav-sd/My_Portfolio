3e0f2dab3a0738e92a80f33c779b56afbe9d2019
saurav-sd <sauravdaware777@gmail.com>
2026-02-23 11:27:50 +0530

    Add About and Contact pages with basic layout

---
+++ app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+      <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">About Page</h1>
+    </section>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+    return (
+      <section className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-semibold">Contact Page</h1>
+    </section>
+  );
+}
